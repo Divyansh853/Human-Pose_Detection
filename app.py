f00dc9a2de44011468eac42e2037bdff386fde0a
Divyansh853 <90828968+Divyansh853@users.noreply.github.com>
2023-12-14 02:48:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, Response
+import cv2
+import cv2 as cv
+import numpy as np
+
+app = Flask(__name__)
+
+BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+
+POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+
+width = 368
+height = 368
+inWidth = width
+inHeight = height
+
+net = cv.dnn.readNetFromTensorflow(r"C:\Users\Divyansh\Desktop\Computer Vision Project\graph_opt.pb")
+thr = 0.2
+
+def poseDetector(frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+    out = net.forward()
+    out = out[:, :19, :, :]
+
+    assert(len(BODY_PARTS) == out.shape[1])
+
+    points = []
+    for i in range(len(BODY_PARTS)):
+        # Slice heatmap of corresponging body's part.
+        heatMap = out[0, i, :, :]
+
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / out.shape[3]
+        y = (frameHeight * point[1]) / out.shape[2]
+        points.append((int(x), int(y)) if conf > thr else None)
+
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert(partFrom in BODY_PARTS)
+        assert(partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+
+        if points[idFrom] and points[idTo]:
+            cv.line(frame, points[idFrom], points[idTo], (255, 0, 0), 3)
+            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+
+    return frame
+
+desired_width = 1280
+desired_height = 720
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen():
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, desired_width)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, desired_height)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        output = poseDetector(frame)
+        _, jpeg = cv2.imencode('.jpg', output)
+        frame_bytes = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
